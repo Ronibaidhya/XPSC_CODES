822d0f094bcb642d76df8b170be0586e90ffa33c
roni <105470281+Ronibaidhya@users.noreply.github.com>
2025-01-06 19:51:55 +0600

    Add files via upload

---
+++ Week_11/Day_01/factory_machine.cpp
@@ -0,0 +1,41 @@
+//https://cses.fi/problemset/task/1620
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+    int n,t;
+    cin>>n>>t;
+    vector<int> v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+
+    auto ok = [&](ll second){
+        ll cnt = 0;
+        for(int i=0;i<n;i++){
+            cnt+=(second/v[i]);
+            if(cnt>=t){
+                return true;
+            }
+        }
+        return false;
+    };
+
+    ll l=1,r=1e18,mid,ans=0;
+    while(l<=r){
+        mid = l+(r-l)/2;
+        if(ok(mid)){
+            ans = mid;
+            r = mid-1;
+        }
+        else{
+            l = mid+1;
+        }
+    }
+    cout<<ans<<'\n';
+    return 0;
+}
+
