7bed5efa93af1896154b2d96b7ac58a7d806d2b1
roni <105470281+Ronibaidhya@users.noreply.github.com>
2025-02-10 22:00:39 +0600

    Add files via upload

---
+++ Week_15/Day_02/permutations.cpp
@@ -0,0 +1,37 @@
+//https://leetcode.com/problems/permutations/
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+class Solution {
+    public:
+        void fun(vector<int>& nums,vector<int>& a,vector<vector<int>> &ans,vector<int> &freq){
+    
+            if(a.size()==nums.size()){
+                ans.push_back(a);
+                return;
+            }
+    
+            for(int i=0;i<freq.size();i++){
+                if(!freq[i]){
+                    a.push_back(nums[i]);
+                    freq[i] = 1;
+                    fun(nums,a,ans,freq);
+                    a.pop_back();
+                    freq[i] = 0;
+                }
+            }
+        }
+        vector<vector<int>> permute(vector<int>& nums) {
+            vector<vector<int>> ans;
+            vector<int> a;
+            vector<int> freq(nums.size(),0);
+            fun(nums,a,ans,freq);
+            return ans;
+        }
+    };
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+    
+    return 0;
+}
